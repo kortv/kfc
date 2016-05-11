6aca393dc9c6182c7b48549d245c00f409fb67d1
kortv <2mothy@gmail.com>
2016-05-11 17:48:10 +0300

    css & scss

--- app/containers/App.js
+++ app/containers/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './../SASS/main.sass';
 import './../SASS/css.css';
+import './../SASS/scss.scss';
 import MainTabl from './../components/MainTabl';
 import LeftBlock from './../components/LeftBlock';
 
