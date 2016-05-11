f6fb786b9cb76fd6e35e85087e43ef28a4396796
pasha_work <pasha.work.it@gmail.com>
2016-05-11 19:51:43 +0300

    table

--- app/components/MainTabl.js
+++ app/components/MainTabl.js
@@ -2,10 +2,27 @@ import React from 'react';
 import TableRow from './TableRow';
 
 export default function MainTabl() {
-  return (
-    <div className="table-container">
-      Таблица
-      <TableRow />
-    </div>
-  );
+    return (
+        <div className="table-container">
+            <table>
+                <tr className="table-header">
+                    <th>№</th>
+                    <th>Тип</th>
+                    <th>Дата заказа</th>
+                    <th>Канал</th>
+                    <th>Заказчик</th>
+                    <th>Город</th>
+                    <th>Ресторан</th>
+                    <th>Тип оплаты заказа</th>
+                    <th>Тип оплаты доставки</th>
+                    <th>Сумма</th>
+                    <th>Блюд в заказе</th>
+                    <th>Время выдачи</th>
+                    <th>Курьер</th>
+                    <th>Статус заказа</th>
+                </tr>
+                <TableRow />
+            </table>
+        </div>
+    );
 }
--- app/components/TableRow.js
+++ app/components/TableRow.js
@@ -1,9 +1,38 @@
 import React from 'react';
 
 export default function TableRow() {
-  return (
-    <div className="table-row">
-      строка
-    </div>
-  );
+    return (
+        <tr className="table-row">
+            <td>12345</td>
+            <td>
+                <span>доставка</span>
+            </td>
+            <td>
+                <span>13:00</span>
+                <span>24.04.2016</span>
+            </td>
+            <td>iOS</td>
+            <td>+79037883181</td>
+            <td>Москва</td>
+            <td>Речной</td>
+            <td>Онлайн</td>
+            <td>Наличные</td>
+            <td>
+                <span>900</span>
+                <span>руб</span>
+            </td>
+            <td>
+                <span>5</span>
+            </td>
+            <td>
+                <span>13:00</span>
+                <span>24.04.2016</span>
+            </td>
+            <td>
+                <span>Красильников Василий</span>
+                <span>курьер на карте</span>
+            </td>
+            <td>Ожидание курьера</td>
+        </tr>
+    );
 }
