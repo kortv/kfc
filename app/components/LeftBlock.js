346db376be04b4a51d421a974640c18bc82c5899
pasha_work <pasha.work.it@gmail.com>
2016-05-16 15:57:40 +0300

    Edit left block. Add new style to main.sass

--- app/components/LeftBlock.js
+++ app/components/LeftBlock.js
@@ -3,7 +3,20 @@ import React from 'react';
 export default function LeftBlock() {
   return (
     <div className="left-menu">
-      Левый столбец
+        <div className=""></div>
+        <div className="">
+            <div className="datepicker"></div>
+            <div className="button-standart"></div>
+            <div className="search"></div>
+            <div className=""></div>
+            <div className=""></div>
+            <div className=""></div>
+            <div className="auth-user">
+                <div className="icon-user"></div>
+                <div className="user-name">Татьяна Лукичева</div>
+                <a className="exit-button">Закончить работу</a>
+            </div>
+        </div>
     </div>
   );
 }
