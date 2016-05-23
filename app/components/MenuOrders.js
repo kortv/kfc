c580f887db4edfdea92863f8edf0cf26becea0ff
pasha_work <pasha.work.it@gmail.com>
2016-05-23 18:22:16 +0300

    edits drop-down

--- app/components/MenuOrders.js
+++ app/components/MenuOrders.js
@@ -28,6 +28,44 @@ export default function MenuOrders(props) {
                 <span>Комментарии&nbsp;</span>
                 <input type='text' name='comments' placeholder='код домофона 1478'/>
             </div>
+            <div className="drop-down deliveryman-list">
+                <div className="title_drop-down">
+                    <span>Доставщик&nbsp;</span>
+                    <span>не указано</span>
+                </div>
+                <div className="content_drop-down">
+                    <div className="deliveryman">
+                        <input id="Gett_Taxi_Currier" className="radio-button" type="radio" name="deliveryman"/>
+                        <label htmlFor="Gett_Taxi_Currier">
+                            <span>Gett Taxi Currier&nbsp;</span>
+                            <span>300 руб</span>
+                        </label>
+                    </div>
+                    <div className="deliveryman">
+                        <input id="Yandex_Taxi_Currier" className="radio-button" type="radio" name="deliveryman"/>
+                        <label htmlFor="Yandex_Taxi_Currier">
+                            <span>Yandex Taxi Currier&nbsp;</span>
+                            <span>300 руб</span>
+                        </label>
+                    </div>
+                    <div className="deliveryman">
+                        <input id="Scooter_Currier" className="radio-button" type="radio" name="deliveryman"/>
+                        <label htmlFor="Scooter_Currier">
+                            <span>Scooter Currier&nbsp;</span>
+                            <span>300 руб</span>
+                        </label>
+                    </div>
+                </div>
+            </div>
+            <div className="drop-down delivery-time">
+                <div className="title_drop-down">
+                    <span>Время доставки заказа&nbsp;</span>
+                    <span>не указано</span>
+                </div>
+                <div className="content_drop-down">
+                    Содержимое
+                </div>
+            </div>
             <div className="payment-method">
                 <div className="payment-cash">
                     <input id="payment-cash" className="radio-button" type="radio" name="payment-method"/>
