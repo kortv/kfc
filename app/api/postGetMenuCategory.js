2bb9495f607ec647d966ec7ca435f2b9a5073434
kortv <2mothy@gmail.com>
2016-05-24 20:36:55 +0300

    sum

---
+++ app/api/postGetMenuCategory.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+export default function postGetMenuCategory(path, body) {
+  axios.post(path, body)
+  .then((data) => {
+    console.log(data);
+    this.setState({
+      orders: data.data.data,
+    });
+  })
+  .catch((response) => {
+    console.log('get data error');
+    console.log(response);
+  });
+}
--- app/containers/App.js
+++ app/containers/App.js
@@ -3,12 +3,14 @@ import MainTablNew from './../components/MainTablNew';
 import LeftBlock from './../components/LeftMenu/LeftBlock';
 import googleMapInit from './../events/googleMapInit';
 import postGetOrders from './../api/postGetOrders';
+import postGetMenuCategory from './../api/postGetMenuCategory';
 import paths from './../_paths';
 
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.postGetOrders = postGetOrders.bind(this);
+    this.postGetMenuCategory = postGetMenuCategory.bind(this);
     this.state = {
       orders: [],
     };
@@ -17,6 +19,12 @@ export default class App extends Component {
   componentDidMount() {
     google.maps.event.addDomListener(window, 'load', googleMapInit);
     this.postGetOrders(paths.main);
+    this.postGetMenuCategory(paths.menuProduct,
+    {"filter":{"type":"and","args":[{"type":"eq","args":["restaurant_id","2995"]},{"type":"eq","args":["category_id",3]}]},"sort":[{"column":"price","descending":false}],"start":0,"count":999}
+/*      { "start":0,"count":999 } */
+/*{"filter":{"type":"eq","args":["restaurant_id","2995"]},"start":0,"count":999}*/
+/*{"filter":{"type":"eq","args":[{"restaurant_id":"2995", "category_id": "5" }]},"start":0,"count":999}
+*/  );
   }
 
   render() {
